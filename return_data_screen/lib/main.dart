19e4c497185b11bfe2d8ef510411bc69aae0d9e6
roberttijani <roberttijani87@gmail.com>
2022-03-21 23:24:53 +0700

    Show the selection screen

--- return_data_screen/lib/main.dart
+++ return_data_screen/lib/main.dart
@@ -98,3 +98,41 @@ class SelectionButton extends StatelessWidget {
     );
   }
 }
+
+class SelectionScreen extends StatelessWidget {
+  const SelectionScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Pick an option'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  // Pop here with "Yep"...
+                },
+                child: const Text('Yep!'),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  // Pop here with "Nope"...
+                },
+                child: const Text('Nope.'),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
