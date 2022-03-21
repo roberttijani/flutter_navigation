d7a31ff64ad5d1f15530d96ba3fd7ae5287341f3
roberttijani <roberttijani87@gmail.com>
2022-03-21 22:54:52 +0700

    Navigate to the widget

--- pass_arguments_named_route/lib/main.dart
+++ pass_arguments_named_route/lib/main.dart
@@ -82,7 +82,7 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
-        child: Column(
+        child: ElevatedButton(
           // Column is also a layout widget. It takes a list of children and
           // arranges them vertically. By default, it sizes itself to fit its
           // children horizontally, and tries to be as tall as its parent.
@@ -97,23 +97,24 @@ class _MyHomePageState extends State<MyHomePage> {
           // center the children vertically; the main axis here is the vertical
           // axis because Columns are vertical (the cross axis would be
           // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+          onPressed: () {
+            // When the user taps the button,
+            // navigate to a named route and
+            // provide the arguments as an optional
+            // parameter.
+            Navigator.pushNamed(
+              context,
+              ExtractArgumentsScreen.routeName,
+              arguments: ScreenArguments(
+                'Extract Arguments Screen',
+                'This message is extracted in the build method.',
+              ),
+            );
+          },
+          child: const Text('Navigate to screen that extracts arguments'),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
