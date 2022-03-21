d42999efd358142567fb1ff0a14119eb70f13f49
roberttijani <roberttijani87@gmail.com>
2022-03-21 23:24:11 +0700

    Add button selection screen

--- return_data_screen/lib/main.dart
+++ return_data_screen/lib/main.dart
@@ -74,3 +74,27 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+
+class SelectionButton extends StatelessWidget {
+  const SelectionButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {
+        _navigateAndDisplaySelection(context);
+      },
+      child: const Text('Pick an option, any option!'),
+    );
+  }
+
+  void _navigateAndDisplaySelection(BuildContext context) async {
+    // Navigator.push returns a Future that completes after calling
+    // Navigator.pop on the Selection Screen.
+    final result = await Navigator.push(
+      context,
+      // Create the SelectionScreen in the next step.
+      MaterialPageRoute(builder: (context) => const SelectionScreen()),
+    );
+  }
+}
